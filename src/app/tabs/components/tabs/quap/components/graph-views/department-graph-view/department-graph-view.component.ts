1749703e1086f395ceaf5fcc9d85433d4e3c7f60
Tamino Walter <tw@digio.ch>
2021-08-16 16:07:07 +0200

    added the graph view

---
+++ src/app/tabs/components/tabs/quap/components/graph-views/department-graph-view/department-graph-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-department-graph-view',
+  templateUrl: './department-graph-view.component.html',
+  styleUrls: ['./department-graph-view.component.scss']
+})
+export class DepartmentGraphViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
