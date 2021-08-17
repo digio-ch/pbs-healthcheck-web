6b6de88eb449a8e3198d0318f5ea7657909b8e61
Tamino Walter <tw@digio.ch>
2021-08-17 10:23:00 +0200

    added data management for the questionnaires and answers

--- src/app/tabs/components/tabs/quap/quap-tab/quap-tab.component.ts
+++ src/app/tabs/components/tabs/quap/quap-tab/quap-tab.component.ts
@@ -3,9 +3,11 @@ import {TabComponent} from "../../../tab/tab.component";
 import {TabService} from "../../../../services/tab.service";
 import {DialogService} from "../../../../../shared/services/dialog.service";
 import {Questionnaire} from "../models/questionnaire";
-import {AnswerStack, AnswerType} from "../models/question";
+import {AnswerOption, AnswerStack, AnswerType} from "../models/question";
 import {PopupService} from "../../../../../shared/services/popup.service";
 import {BehaviorSubject} from "rxjs";
+import {AnswerState} from '../store/answer.state';
+import {QuestionnaireState} from '../store/questionnaire.state';
 
 @Component({
   selector: 'app-quap-tab',
@@ -81,12 +83,48 @@ export class QuapTabComponent extends TabComponent implements OnInit {
     protected tabService: TabService,
     private dialogService: DialogService,
     private popupService: PopupService,
+    private questionnaireState: QuestionnaireState,
+    private answerState: AnswerState,
   ) {
     super(tabService, QuapTabComponent);
   }
 
   ngOnInit(): void {
     this.answers = {};
+
+    // TODO example data replace with real data
+    this.questionnaireState.setQuestionnaire({
+      id: 1,
+      aspects: [
+        {id: 1, name: 'Programm', questions: []},
+        {id: 2, name: 'Erfüllung', questions: []},
+        {id: 3, name: 'Zufriedenheit', questions: []},
+        {id: 4, name: 'Image', questions: []},
+        {id: 5, name: 'Werbung', questions: []},
+        {id: 6, name: 'Ressourcen', questions: []},
+        {id: 7, name: 'Zahl', questions: []},
+        {id: 8, name: 'Ausstausch', questions: []},
+        {id: 9, name: 'Netzwerk', questions: []},
+        {id: 10, name: 'Bildung', questions: []},
+        {id: 11, name: 'Motivation', questions: []},
+        {id: 12, name: 'Kultur', questions: []},
+        {id: 13, name: 'Team', questions: []},
+        {id: 14, name: 'Programm', questions: []},
+        {id: 15, name: 'Profil', questions: []},
+        {id: 16, name: 'Betreuung', questions: []},
+      ]
+    });
+
+    // TODO example data replace with real data
+    this.answerState.setAnswers({
+      1: {
+        1: AnswerOption.FULLY_APPLIES,
+        2: AnswerOption.FULLY_APPLIES,
+        3: AnswerOption.NOT_ANSWERED,
+        4: AnswerOption.SOMEWHAT_APPLIES,
+        5: AnswerOption.NOT_RELEVANT,
+      }
+    });
   }
 
   openEvaluationDialog(): void {
---
+++ src/app/tabs/components/tabs/quap/store/questionnaire.state.ts
@@ -0,0 +1,24 @@
+import {Injectable} from "@angular/core";
+import {Questionnaire} from "../models/questionnaire";
+import {BehaviorSubject, Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionnaireState {
+
+  private questionnaire = new BehaviorSubject<Questionnaire>(null);
+
+  getQuestionnaire$(): Observable<Questionnaire> {
+    return this.questionnaire.asObservable();
+  }
+
+  getQuestionnaire(): Questionnaire {
+    return this.questionnaire.value;
+  }
+
+  setQuestionnaire(questionnaire: Questionnaire): void {
+    this.questionnaire.next(questionnaire);
+  }
+
+}
