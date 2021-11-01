dfc51ec50287ee7fa72a8832d72f8b42182ec2af
Ciro Brodmann <cb@digio.ch>
2021-11-01 16:06:37 +0100

    changed QUAP tab title

--- src/app/tabs/components/tabs/quap/components/detail-view/detail-view.component.ts
+++ src/app/tabs/components/tabs/quap/components/detail-view/detail-view.component.ts
@@ -2,7 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {DialogService} from '../../../../../../shared/services/dialog.service';
 import {Aspect} from '../../models/aspect';
 import {AnswerStack, Question} from '../../models/question';
-import {Help} from "../../models/help";
+import {Help} from '../../models/help';
 import {Link} from '../../models/link';
 
 @Component({
