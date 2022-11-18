d4e044b40a838f3d526f773cfe41adffd8a797c2
Sebastian <ss@digio.ch>
2022-11-18 09:17:36 +0100

    UX improvments to QUAP Settings.

--- src/app/apps/quap/components/settings-view/settings-view.component.ts
+++ src/app/apps/quap/components/settings-view/settings-view.component.ts
@@ -1,25 +1,28 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {DialogService} from '../../../../shared/services/dialog.service';
+import {DialogController, DialogService} from '../../../../shared/services/dialog.service';
 import {QuapSettings, QuapSettingsService} from '../../services/quap-settings.service';
 import {ApiService} from '../../../../shared/services/api.service';
 import {GroupFacade} from '../../../../store/facade/group.facade';
 import {Group} from '../../../../shared/models/group';
+import {PopupService, PopupType} from '../../../../shared/services/popup.service';
 
 @Component({
   selector: 'app-settings-view',
   templateUrl: './settings-view.component.html',
   styleUrls: ['./settings-view.component.scss']
 })
-export class SettingsViewComponent implements OnInit {
+export class SettingsViewComponent implements OnInit, DialogController {
   @Input() disableGroupToggles = false;
 
   settings: QuapSettings;
+  wasModified: boolean = false;
 
   constructor(
     private dialogService: DialogService,
     private quapSettingsService: QuapSettingsService,
     private groupFacade: GroupFacade,
     private apiService: ApiService,
+    private popupService: PopupService,
   ) { }
 
   get canton(): string {
@@ -27,14 +30,17 @@ export class SettingsViewComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.dialogService.addDialogController(this);
     this.settings = JSON.parse(JSON.stringify(this.quapSettingsService.getSettingsSnapshot()));
   }
 
   updateShowNotRelevant(value: boolean): void {
+    this.wasModified = true;
     this.settings.showNotRelevant = value;
   }
 
   updateShareData(value: boolean): void {
+    this.wasModified = true;
     this.settings.shareData = value;
 
     this.apiService.patch(`groups/${(this.groupFacade.getCurrentGroupSnapshot().id)}/quap/questionnaire`, {
@@ -43,13 +49,36 @@ export class SettingsViewComponent implements OnInit {
   }
 
   close(): void {
-    this.quapSettingsService.setSettings(this.settings);
+    this.dialogService.close().then();
+  }
 
-    this.dialogService.close();
+  onCloseRequest(): Promise<boolean> {
+    if (!this.wasModified){
+      return Promise.resolve(true);
+    }
+
+    return this.popupService.open({
+      title: 'dialog.quap.unsaved_changes.title',
+      message: 'dialog.quap.unsaved_changes.message',
+      cancel: 'dialog.quap.unsaved_changes.cancel',
+      submit: 'dialog.quap.unsaved_changes.submit',
+      type: PopupType.WARNING,
+    });
+  }
+
+  save(): void {
+    this.quapSettingsService.setSettings(this.settings);
+    this.dialogService.forceClose();
   }
 
   isOwner(): boolean {
     return this.groupFacade.getCurrentGroupSnapshot().permissionType === Group.PERMISSION_TYPE_OWNER;
+  }
+
+  afterClosed(result: any): void {
+  }
+
+  beforeClosed(result: any): void {
   }
 
 }
