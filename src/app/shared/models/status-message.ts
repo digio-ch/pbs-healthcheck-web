0e2eae7aa8e56f8eaa6d674ec3b62e806af88668
Léonard Roch <lr@digio.ch>
2025-02-12 08:03:12 +0100

    refactored severity to enum type

--- src/app/shared/models/status-message.ts
+++ src/app/shared/models/status-message.ts
@@ -1,11 +1,11 @@
-export type Severity = 'none' | 'info' | 'warning' | 'error';
+export enum Severity {
+    None = 'none',
+    Info = 'info',
+    Warning = 'warning',
+    Error = 'error'
+}
 
 export class StatusMessage {
-  public static SEVERITY_NONE: Severity = 'none';
-  public static SEVERITY_INFO: Severity = 'info';
-  public static SEVERITY_WARNING: Severity = 'warning';
-  public static SEVERITY_ERROR:Severity = 'error';
-
   constructor(
     public severity: Severity,
     public title: string,
